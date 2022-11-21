dfa03f815c1510aaa463e1706f699606dfa4e099
CEASER2116 <20210345@itla.edu.do>
2022-11-21 00:14:25 -0400

    todo

---
+++ CODIGO 3.0/Programa 3.3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+/* Suma pagos.
+El programa, al recibir como datos un conjunto de pagos realizados en el último
+mes, obtiene la suma de los mismos.
+PAG y SPA: variables de tipo real. */
+
+
+
+void main(void)
+{
+    float PAG, SPA;
+    SPA=0;
+
+
+
+  printf ("ingrse el numero de pago: \t");
+
+    scanf("%f", &PAG);
+    while (PAG)
+
+    /* Observa que la condición es verdadera mientras el pago es diferente de cero. */
+{
+
+    SPA = SPA + PAG;
+printf("Ingrese el siguiente pago:\t ");
+
+scanf("%f",&PAG);
+
+
+/* Observa que la proposición que modifica la condición es una lectura. */
+}
+printf("\nEl total de pagos del mes es: %.2f", SPA);
+}
